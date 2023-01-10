cceb07eabda5cbcf2ccc1ddeb157eeeb5086cb9b
winxene <winxeneric@gmail.com>
2023-01-10 12:11:15 +0700

    feat: add timer and reveal qrcode string

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import qrcode from 'qrcode';
+import CopyToClipboard from 'copy-to-clipboard';
+import {IconButton} from '@material-ui/core';
+import CopyIcon from '@material-ui/core/Icon/Icon';
 import { initializeApp } from 'firebase/app' // no compat for new SDK
 import { getDatabase, ref, set, update } from 'firebase/database'
 
 const HomePage = () => {
   const [qrCodeUrl, setQRCodeUrl] = React.useState(null);
+  const [showQRCode, setShowQRCode] = React.useState(true);
+  const [showTokenString, setShowTokenString] = React.useState(false);
   //randomize 16 string
   function generateString(length) {
     var result           = '';
@@ -15,19 +20,8 @@ const HomePage = () => {
     }
     return result;
   }
-
   const tokenStringData = generateString(16);
-  const firebaseConfig = {
-      // Your Firebase configuration here
-        apiKey: "AIzaSyByAJxUcwo5ldPwnGNHLrIkPSPioihwVZ4",
-        authDomain: "drinkify-firebase.firebaseapp.com",
-        databaseURL: "https://drinkify-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "drinkify-firebase",
-        storageBucket: "drinkify-firebase.appspot.com",
-        messagingSenderId: "963701895505",
-        appId: "1:963701895505:web:cf7cb4440df731541da370",
-        measurementId: "G-G4VQZ01XGP"
-    };
+  const firebaseConfig = require('../config/firebaseConfig.ts');
   const app = initializeApp(firebaseConfig);
   const drinkifyDatabase = getDatabase(app);  
   const drinkifyTokenRef = ref(drinkifyDatabase, '/balanceToken');
@@ -37,12 +31,23 @@ const HomePage = () => {
       update(drinkifyTokenRef, {
         "5k": tokenStringData
     });
+    setShowQRCode(true);
+    setTimeout(() => {
+      setShowQRCode(false);
+    }, 30000);
   });
 };
   return (
     <div className="flex justify-center items-center h-screen p-4">
       <button onClick={generateQRCode}>Generate QR code</button>
-      {qrCodeUrl ? <img src={qrCodeUrl} /> : <p>Loading QR code...</p>}
+      {showQRCode && qrCodeUrl ? <img src={qrCodeUrl} /> : <p>QR code will be shown here</p>}
+      <button onClick={() => setShowTokenString(!showTokenString)}> show token string </button>
+      { showTokenString && (
+        <div>
+        <p>{tokenStringData}</p>
+        <IconButton onClick={() => CopyToClipboard(tokenStringData)}><CopyIcon /></IconButton>
+        </div>
+      )}
     </div>
   );
 }
