d3c28833954512c7ef2303b6eadb89307b87b998
winxene <winxeneric@gmail.com>
2023-01-19 15:34:50 +0700

    feat: added firebaseConfig

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+      // Your Firebase configuration here
+        apiKey: "AIzaSyByAJxUcwo5ldPwnGNHLrIkPSPioihwVZ4",
+        authDomain: "drinkify-firebase.firebaseapp.com",
+        databaseURL: "https://drinkify-firebase-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "drinkify-firebase",
+        storageBucket: "drinkify-firebase.appspot.com",
+        messagingSenderId: "963701895505",
+        appId: "1:963701895505:web:cf7cb4440df731541da370",
+        measurementId: "G-G4VQZ01XGP"
+    };
+
+module.exports = firebaseConfig;
